ba7f1ed3a2272d3b2bb61609bd7ca41197139f64
mahmoudadev <mahmoud.abdelmoneim.dev@gmail.com>
2018-04-21 19:54:42 +0200

    modifying database schema

---
+++ database/migrations/2018_04_21_172201_create_reservation_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('paidPrice');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('room_id');
+            $table->unsignedInteger('employee_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->foreign('employee_id')->references('id')->on('employees');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation');
+    }
+}
