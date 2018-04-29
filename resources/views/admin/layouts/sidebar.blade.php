c6926edb2cb59ded76ab55c821b4307dd8d94a24
Khaled Fahmy <khaledfahmy219@gmail.com>
2018-04-29 14:32:03 +0200

    complete side bar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -9,7 +9,8 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src=" {{ Storage::url('images/3.jpg') }} " class="img-circle" alt="User Image">
+                
+                <img src=" {{ Storage::url(Auth::guard('employee')->user()->image) }} " class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
                 <p>  {{ ucfirst( Auth::guard('employee')->user()->name ) }}</p>
@@ -35,15 +36,24 @@
             <li class="header">HEADER</li>
             <!-- Optionally, you can add icons to the links -->
 
-            @hasRole("Admin")
+            @hasrole('Admin')
             <li {{ Request::route('Managers.index') ? 'class="active"' : null}}><a href="{{ route('Managers.index') }}"><i class="fa fa-link"></i> <span>Manage Managers</span></a></li>
-            
+            @endhasrole
+            @hasrole('Admin|Manager')
             <li class=""><a href="{{ route('Receptionists.index') }}"><i class="fa fa-link"></i> <span>Manage Receptionists</span></a></li>
+            @endhasrole
+            @hasrole('Admin|Manager|Receptionist')
             <li class=""><a href="{{ route('clients.index') }}"><i class="fa fa-link"></i> <span>Manage Clients</span></a></li>
+            @endhasrole
+            @hasrole('Admin|Manager')
             <li class=""><a href="{{ route('floors.index') }}"><i class="fa fa-link"></i> <span>Manage Floors</span></a></li>
             <li class=""><a href="{{ route('rooms.index') }}"><i class="fa fa-link"></i> <span>Manage Rooms</span></a></li>
+            @endhasrole
+            @hasrole('Admin|Manager|Receptionist')
             <li class=""><a href="{{ route('clients.approved') }}"><i class="fa fa-link"></i> <span>Approved clients</span></a></li>
             <li class=""><a href="{{  route('clients.reservation') }}"><i class="fa fa-link"></i> <span>Clients Reservations​</span></a></li>
+            <li class=""><a href="{{  route('clients.pending') }}"><i class="fa fa-link"></i> <span>Clients Pending​</span></a></li>
+            @endhasrole
             <li class="treeview">
                 <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
                     <span class="pull-right-container">
@@ -52,10 +62,10 @@
                 </a>
                 <ul class="treeview-menu">
                     <li><a href="#">Link in level 2</a></li>
-                    <li><a href="#">Link in level 2</a></li
+                    <li><a href="#">Link in level 2</a></li>
                 </ul>
-            </li>
-        </ul>
+            
+                </ul>
         <!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
