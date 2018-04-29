fef16089fb05e206d2b53cc1b4318b5e39b39a83
Khaled Fahmy <khaledfahmy219@gmail.com>
2018-04-29 17:16:39 +0200

    handle middlreware for roles

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -34,35 +34,38 @@
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">HEADER</li>
             <!-- Optionally, you can add icons to the links -->
+           
+            <!-- @hasRole("Admin") -->
+                <li><a href="{{ route('Managers.index') }}"><i class="fa fa-link"></i> <span>Manage Managers</span></a></li>
+            <!-- @endhasRole -->
 
-            @hasrole('Admin')
-            <li {{ Request::route('Managers.index') ? 'class="active"' : null}}><a href="{{ route('Managers.index') }}"><i class="fa fa-link"></i> <span>Manage Managers</span></a></li>
-            @endhasrole
-            @hasrole('Admin|Manager')
-            <li class=""><a href="{{ route('Receptionists.index') }}"><i class="fa fa-link"></i> <span>Manage Receptionists</span></a></li>
-            @endhasrole
-            @hasrole('Admin|Manager|Receptionist')
-            <li class=""><a href="{{ route('clients.index') }}"><i class="fa fa-link"></i> <span>Manage Clients</span></a></li>
-            @endhasrole
-            @hasrole('Admin|Manager')
-            <li class=""><a href="{{ route('floors.index') }}"><i class="fa fa-link"></i> <span>Manage Floors</span></a></li>
-            <li class=""><a href="{{ route('rooms.index') }}"><i class="fa fa-link"></i> <span>Manage Rooms</span></a></li>
-            @endhasrole
-            @hasrole('Admin|Manager|Receptionist')
-            <li class=""><a href="{{ route('clients.approved') }}"><i class="fa fa-link"></i> <span>Approved clients</span></a></li>
-            <li class=""><a href="{{  route('clients.reservation') }}"><i class="fa fa-link"></i> <span>Clients Reservations​</span></a></li>
-            <li class=""><a href="{{  route('clients.pending') }}"><i class="fa fa-link"></i> <span>Clients Pending​</span></a></li>
-            @endhasrole
+            <!-- @hasRole('Admin|Manager') -->
+                 <li class=""><a href="{{ route('Receptionists.index') }}"><i class="fa fa-link"></i> <span>Manage Receptionists</span></a></li>
+            <!-- @endhasRole -->
+
+            <!-- @hasRole('Admin|Manager|Receptionist') -->
+                <li class=""><a href="{{ route('clients.index') }}"><i class="fa fa-link"></i> <span>Manage Clients</span></a></li>
+            <!-- @endhasRole -->
+            <!-- @hasRole('Admin|Manager') -->
+                <li class=""><a href="{{ route('floors.index') }}"><i class="fa fa-link"></i> <span>Manage Floors</span></a></li>
+                <li class=""><a href="{{ route('rooms.index') }}"><i class="fa fa-link"></i> <span>Manage Rooms</span></a></li>
+            <!-- @endhasRole -->
+            <!-- @hasRole('Admin|Manager|Receptionist') -->
+                <li class=""><a href="{{ route('clients.approved') }}"><i class="fa fa-link"></i> <span>Approved clients</span></a></li>
+                <li class=""><a href="{{  route('clients.reservation') }}"><i class="fa fa-link"></i> <span>Clients Reservations​</span></a></li>
+                <li class=""><a href="{{  route('clients.pending') }}"><i class="fa fa-link"></i> <span>Clients Pending​</span></a></li>
+            <!-- @endhasRole -->
             <li class="treeview">
                 <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
                     <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
                 </a>
                 <ul class="treeview-menu">
                     <li><a href="#">Link in level 2</a></li>
                     <li><a href="#">Link in level 2</a></li>
                 </ul>
+            </li>    
             
                 </ul>
         <!-- /.sidebar-menu -->
--- routes/web.php
+++ routes/web.php
@@ -62,47 +62,47 @@ Route::post('clients/delete','ManagerController@delete')->name('clients.destroy'
 
 
 Route::group([
-  'middleware'=>'employees','role:admin|manager'
+  'middleware'=>'employees','role' => 'Admin|Manager'
 ],
 function () {
-Route::get('Receptionists', 'ManagerReceptController@index')->name('Receptionists.index');
-Route::get('ReceptionistsData', 'ManagerReceptController@get_data')->name('ReceptionistsData');
-Route::post('Receptionists/{id}/ban', 'ManagerReceptController@Ban')->name('Receptionists.ban');
-Route::get('Receptionists/create', 'ManagerReceptController@create')->name('Receptionists.create');
-Route::post('Receptionists/ban', 'ManagerReceptController@Ban')->name('Receptionists.ban');
-Route::post('Receptionists/store', 'ManagerReceptController@store')->name('Receptionists.store');
-Route::get('Receptionists/{id}/edit','ManagerReceptController@edit')->name('Receptionists.edit');
-Route::patch('Receptionists/{id}/update', 'ManagerReceptController@update')->name('Receptionists.update');
-Route::post('Receptionists/delete','ManagerReceptController@delete')->name('Receptionists.delete');
-
-Route::get('floors', 'ManagerFloorController@index')->name('floors.index');
-Route::get('floorsData', 'ManagerFloorController@get_data')->name('floorsData');
-Route::get('floors/create', 'ManagerFloorController@create')->name('floors.create');
-Route::post('floors/store', 'ManagerFloorController@store')->name('floors.store');
-Route::get('floors/{id}/edit','ManagerFloorController@edit')->name('floors.edit');
-Route::patch('floors/{id}/update', 'ManagerFloorController@update')->name('floors.update');
-Route::post('floors/delete','ManagerFloorController@delete')->name('floors.delete');
-
-Route::get('rooms', 'ManagerRoomController@index')->name('rooms.index');
-Route::get('roomsData', 'ManagerRoomController@get_data')->name('roomsData');
-Route::get('rooms/create', 'ManagerRoomController@create')->name('rooms.create');
-Route::post('rooms/store', 'ManagerRoomController@store')->name('rooms.store');
-Route::get('rooms/{id}/edit','ManagerRoomController@edit')->name('rooms.edit');
-Route::patch('rooms/{id}/update', 'ManagerRoomController@update')->name('rooms.update');
-Route::post('rooms/delete','ManagerRoomController@delete')->name('rooms.delete');
 });
 
-Route::group([
-  'middleware'=>'employees','role:admin'
-],
-function () {
-Route::get('Managers', 'ManagerManagerController@index')->name('Managers.index');
-Route::get('ManagersData', 'ManagerManagerController@get_data')->name('ManagersData');
-Route::get('Managers/create', 'ManagerManagerController@create')->name('Managers.create');
-Route::post('Managers/store', 'ManagerManagerController@store')->name('Managers.store');
-Route::get('Managers/{id}/edit','ManagerManagerController@edit')->name('Managers.edit');
-Route::patch('Managers/{id}/update', 'ManagerManagerController@update')->name('Managers.update');
-Route::post('Managers/delete','ManagerManagerController@delete')->name('Managers.delete');
+Route::group(['middleware'=>'employees'],function () {
+    Route::get('Managers', 'ManagerManagerController@index')->name('Managers.index');
+    Route::get('ManagersData', 'ManagerManagerController@get_data')->name('ManagersData');
+    Route::get('Managers/create', 'ManagerManagerController@create')->name('Managers.create');
+    Route::post('Managers/store', 'ManagerManagerController@store')->name('Managers.store');
+    Route::get('Managers/{id}/edit','ManagerManagerController@edit')->name('Managers.edit');
+    Route::patch('Managers/{id}/update', 'ManagerManagerController@update')->name('Managers.update');
+    Route::post('Managers/delete','ManagerManagerController@delete')->name('Managers.delete');  
+    
+    Route::get('Receptionists', 'ManagerReceptController@index')->name('Receptionists.index');
+    Route::get('ReceptionistsData', 'ManagerReceptController@get_data')->name('ReceptionistsData');
+    Route::post('Receptionists/{id}/ban', 'ManagerReceptController@Ban')->name('Receptionists.ban');
+    Route::get('Receptionists/create', 'ManagerReceptController@create')->name('Receptionists.create');
+    Route::post('Receptionists/ban', 'ManagerReceptController@Ban')->name('Receptionists.ban');
+    Route::post('Receptionists/store', 'ManagerReceptController@store')->name('Receptionists.store');
+    Route::get('Receptionists/{id}/edit','ManagerReceptController@edit')->name('Receptionists.edit');
+    Route::patch('Receptionists/{id}/update', 'ManagerReceptController@update')->name('Receptionists.update');
+    Route::post('Receptionists/delete','ManagerReceptController@delete')->name('Receptionists.delete');
+
+    Route::get('floors', 'ManagerFloorController@index')->name('floors.index');
+    Route::get('floorsData', 'ManagerFloorController@get_data')->name('floorsData');
+    Route::get('floors/create', 'ManagerFloorController@create')->name('floors.create');
+    Route::post('floors/store', 'ManagerFloorController@store')->name('floors.store');
+    Route::get('floors/{id}/edit','ManagerFloorController@edit')->name('floors.edit');
+    Route::patch('floors/{id}/update', 'ManagerFloorController@update')->name('floors.update');
+    Route::post('floors/delete','ManagerFloorController@delete')->name('floors.delete');
+
+    Route::get('rooms', 'ManagerRoomController@index')->name('rooms.index');
+    Route::get('roomsData', 'ManagerRoomController@get_data')->name('roomsData');
+    Route::get('rooms/create', 'ManagerRoomController@create')->name('rooms.create');
+    Route::post('rooms/store', 'ManagerRoomController@store')->name('rooms.store');
+    Route::get('rooms/{id}/edit','ManagerRoomController@edit')->name('rooms.edit');
+    Route::patch('rooms/{id}/update', 'ManagerRoomController@update')->name('rooms.update');
+    Route::post('rooms/delete','ManagerRoomController@delete')->name('rooms.delete');
+
+
 });
 
 Route::get('reservation/room/{id}', 'RoomsController@index')->name('booking');
